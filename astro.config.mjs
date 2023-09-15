ce3a8e992bdb4facae53c4f517eb943c43e73f6a
Jorge <rack1992@gmail.com>
2023-09-15 14:31:42 -0500

    Base

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import {
+  defineConfig
+} from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import relativeLinks from "astro-relative-links";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [relativeLinks(), tailwind()],
+  site: "https://isource.us/",
+  base: "dev/WebAstroVersion/"
+});
---
+++ src/pages/blog/articulo-blog.md
@@ -0,0 +1,20 @@
+---
+title: "Titulo del articulo"
+layout: "../../layouts/Layout.astro"
+---
+
+# Esto es un ejemplo de articulo
+
+lorem ipsum dolor sit amet
+
+## Subtitulo
+--
+lorem ipsum
+
+## subtitulo 2
+
+```javascript
+function (){
+    console.log();
+}
+```
