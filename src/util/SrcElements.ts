bcad7365c203c354575942fd66cfba21ba429113
Jorge <rack1992@gmail.com>
2023-09-19 18:22:35 -0500

    Se implementa el Hero y cargan algunas imagenes

---
+++ src/util/SrcElements.ts
@@ -0,0 +1,10 @@
+import HeaderLogo_Img from "../img/Logo/White/Logo-NUWinds-07.webp";
+import FavIcon_Img from "../img/FavIcon/32px/FAV Icon NUwinds-06.png";
+
+export function HeaderLogo() {
+  return HeaderLogo_Img;
+}
+
+export function FavIcon() {
+  return FavIcon_Img;
+}
---
+++ src/util/Urls.ts
@@ -0,0 +1,7 @@
+// const websiteUrl = "www.google.com";
+const websiteUrl = "http://localhost:4321/";
+
+export function WebSite ()
+{
+    return websiteUrl;
+}
