5ff88e309678cd42620c0d26df28d2c78b53e418
Jorge <rack1992@gmail.com>
2023-09-26 19:57:58 -0500

    Se organiza carpetas y se avanza con servicios

--- src/util/SrcElements.ts
+++ src/util/SrcElements.ts
@@ -1,5 +1,6 @@
-import HeaderLogo_Img from "../img/Logo/White/Logo-NUWinds-07.webp";
+import HeaderLogo_Img from "../img/Logo/White/Logo-NUWinds-07.png";
 import FavIcon_Img from "../img/FavIcon/32px/FAV Icon NUwinds-06.png";
+import DogImg_Img from "../img/Logo/Logo-NUWinds-06.png";
 
 export function HeaderLogo() {
   return HeaderLogo_Img;
@@ -7,4 +8,8 @@ export function HeaderLogo() {
 
 export function FavIcon() {
   return FavIcon_Img;
+}
+
+export function DogImg() {
+  return DogImg_Img;
 }
